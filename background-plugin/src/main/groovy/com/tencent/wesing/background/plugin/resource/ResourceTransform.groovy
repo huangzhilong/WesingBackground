719fb05f92c50f61484ad9af899a3fc82336d81f
zlonghuang <964123660@qq.com>
2021-05-07 16:33:50 +0800

    Transform实现

--- background-plugin/src/main/groovy/com/tencent/wesing/background/plugin/resource/ResourceTransform.groovy
+++ background-plugin/src/main/groovy/com/tencent/wesing/background/plugin/resource/ResourceTransform.groovy
@@ -57,50 +57,124 @@ class ResourceTransform extends Transform {
         // 创建一个对应名称表示的输出目录 位于app/build/intermediates/transforms/(transform的getName)
         // 是从0 、1、2开始递增。如果是目录，名称就是对应的数字，如果是jar包就类似0.jar
         TransformOutputProvider mOutputProvider = transformInvocation.getOutputProvider()
+        //此次是否是增量
+        boolean isIncremental = transformInvocation.isIncremental()
+
+        //非增量清空旧的输出内容
+        if (!isIncremental) {
+            mOutputProvider.deleteAll()
+        }
         transformInvocation.inputs.each { TransformInput input ->
             input.jarInputs.each { JarInput jarInput ->
                 //处理Jar
-                processJarInput(jarInput, mOutputProvider)
+                processJarInput(jarInput, mOutputProvider, isIncremental)
             }
 
             input.directoryInputs.each { DirectoryInput directoryInput ->
                 //处理源码文件
-                processDirectoryInputs(directoryInput, mOutputProvider)
+                processDirectoryInputs(directoryInput, mOutputProvider, isIncremental)
             }
         }
     }
 
-    private void processJarInput(JarInput jarInput, TransformOutputProvider outputProvider) {
+    private void processJarInput(JarInput jarInput, TransformOutputProvider outputProvider, boolean isIncremental) {
         File dest = outputProvider.getContentLocation(
                 jarInput.getFile().getAbsolutePath(),
                 jarInput.getContentTypes(),
                 jarInput.getScopes(),
                 Format.JAR)
+        if (isIncremental) {
+            Status status = jarInput.getStatus()
+            switch (status) {
+                case Status.NOTCHANGED:
+                    break
+                case Status.REMOVED:
+                    if (dest.exists()) {
+                        FileUtils.delete(dest)
+                    }
+                    break
+                case Status.CHANGED:
+                case Status.ADDED:
+                    doTransformJar(jarInput, dest)
+                    break
+                default:
+                    break
+            }
+        } else  {
+            doTransformJar(jarInput, dest)
+        }
+    }
 
+    private void doTransformJar(JarInput jarInput, File dest) {
         //将修改过的字节码copy到dest，就可以实现编译期间干预字节码的目的了
-
         LogUtil.logI(TAG, "processJarInput fileName: ${jarInput.getFile().absolutePath}  ${jarInput.name}  dest: ${dest.absolutePath}")
         FileUtils.copyFile(jarInput.getFile(), dest)
     }
 
-    private void processDirectoryInputs(DirectoryInput directoryInput, TransformOutputProvider outputProvider) {
+    private void processDirectoryInputs(DirectoryInput directoryInput, TransformOutputProvider outputProvider, boolean isIncremental) {
         File dest = outputProvider.getContentLocation(directoryInput.getName(),
                 directoryInput.getContentTypes(), directoryInput.getScopes(),
                 Format.DIRECTORY)
         //建立文件夹
         FileUtils.mkdirs(dest)
+        if (isIncremental) {
+            Map<File, Status> statusMap = directoryInput.getChangedFiles()
+            Iterator<File> iterator = statusMap.keySet().iterator()
+            //输出目录文件夹地址和输入文件夹地址
+            String destPath = dest.getAbsolutePath()
+            String directoryPath = directoryInput.file.getAbsolutePath()
+            while (iterator.hasNext()) {
+                File file = iterator.next()
+                Status status = statusMap.get(file)
+                //得到输出目录的文件路径
+                String fileDestPath = file.getAbsolutePath().replace(directoryPath, destPath)
+                File destFile = new File(fileDestPath)
+                switch (status) {
+                    case Status.NOTCHANGED:
+                        break
+                    case Status.REMOVED:
+                        if (destFile.exists()) {
+                            FileUtils.delete(destFile)
+                        }
+                        break
+                    case Status.ADDED:
+                    case Status.CHANGED:
+                        //操作修改对输入file，然后copy到输出目录
+                        onHandleDirectoryEachFile(directoryInput.name, file)
+                        FileUtils.copyFile(file, destFile)
+                        break
+                    default:
+                        break
+                }
+            }
+        } else {
+            //遍历操作每个文件再进行copy的输出目录
+            eachFileToDirectory(directoryInput.name, directoryInput.file)
+            FileUtils.copyDirectory(directoryInput.file, dest)
+        }
+    }
 
-        LogUtil.logI(TAG, "processDirectoryInputs fileName: ${directoryInput.getFile().absolutePath}  ${directoryInput.name}  dest: ${dest.absolutePath}")
-        //需要使用文件夹遍历，这种只针对增量
-//        Map<File, Status> statusMap = directoryInput.getChangedFiles()
-//        Iterator<File> iterator = statusMap.keySet().iterator()
-//        while (iterator.hasNext()) {
-//            File file = iterator.next()
-//            Status status = statusMap.get(file)
-//            LogUtil.logI(TAG, "find BackgroundShapeConfig path: ${file.absolutePath}")
-//        }
+    private void eachFileToDirectory(String name, File file) {
+        if (file == null || !file.exists()) {
+            return
+        }
+        if (file.isDirectory()) {
+            File [] fileList = file.listFiles()
+            for (int i = 0; i < fileList.length; i++) {
+                File subFile = fileList[i]
+                if (subFile.isDirectory()) {
+                    eachFileToDirectory(name, subFile)
+                } else {
+                    onHandleDirectoryEachFile(name, subFile)
+                }
+            }
+        } else {
+            onHandleDirectoryEachFile(name, file)
+        }
+    }
 
-        //将修改过的字节码copy到dest，就可以实现编译期间干预字节码的目的了
-        FileUtils.copyDirectory(directoryInput.getFile(), dest)
+
+    private void onHandleDirectoryEachFile(String name, File file) {
+        LogUtil.logI(TAG, "onHandleDirectoryEachFile name: $name  fileName: ${file.name}")
     }
 }
